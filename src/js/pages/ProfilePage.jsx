62826ed2e9c0ec12bfd653667390d9d7df0f80e1
molnard1 <101341428+molnard1@users.noreply.github.com>
2023-11-10 10:18:03 +0100

    Changes

--- src/js/pages/ProfilePage.jsx
+++ src/js/pages/ProfilePage.jsx
@@ -2,24 +2,47 @@ import React, { useEffect, useState } from "react";
 import { API_URL } from "../Globals";
 
 export default function ProfilePage() {
-    const [profileData, setProfileData] = useState([]);
+    const [profileData, setProfileData] = useState({
+      profile: [],
+      inventory: []
+    });
 
     useEffect(() => {
         const fetchData = async () => {
-          const response = await fetch(`${API_URL}/profile`, {
+          const profileResponse = await fetch(`${API_URL}/profile`, {
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem("accessToken")}`
             }
           });
-          const json = await response.json();
-          setProfileData(json);
+
+          const inventoryResponse = await fetch(`${API_URL}/inventory`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("accessToken")}`
+            }
+          });
+          const profileJSON = await profileResponse.json();
+          const inventoryJSON = await inventoryResponse.json();
+          setProfileData({
+            profile: profileJSON,
+            inventory: inventoryJSON
+          });
         };
     
         fetchData();
       }, []);
 
     return [
-        <h1>Hello {profileData.username}!</h1>,
-        <h1>Your balance is: {profileData.balance}.</h1>
+        <h1>Hello {profileData.profile.username}!</h1>,
+        <h1>Your balance is: {profileData.profile.balance}.</h1>,
+        <div>
+          <h2>Here is a list of all your inventory items: ({profileData.inventory.length} items)</h2>
+          {profileData.inventory.map(item => {
+            return [
+            <div>
+
+            </div>
+            ]
+          })}
+        </div>
     ]
 }
