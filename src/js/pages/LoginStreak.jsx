89589d098a8554012796024c02230c71341c0975
molnard1 <101341428+molnard1@users.noreply.github.com>
2024-02-16 09:47:08 +0100

    login streak changes

--- src/js/pages/LoginStreak.jsx
+++ src/js/pages/LoginStreak.jsx
@@ -11,13 +11,9 @@ export default function LoginStreak() {
     const todayDate = dayjs().date();
     const firstDayIndex = startOfMonth.day();
 
-    let monthDays = [];
-    for (let i = 0; i < firstDayIndex; i++) {
-        monthDays.push(null);
-    }
-    for (let i = 1; i <= daysInMonth; i++) {
-        monthDays.push(i);
-    }
+    const monthDays = Array.from({ length: firstDayIndex }, () => null).concat(
+        Array.from({ length: daysInMonth }, (_, i) => i + 1)
+    );
 
     const [userClicked, setUserClicked] = useState([]);
     const [claimSuccess, setClaimSuccess] = useState(false);
@@ -31,12 +27,16 @@ export default function LoginStreak() {
 
     return (
         <div>
-            <div>
-            {claimSuccess &&<Notification color="lime" radius="lg" title="Sikeresen kiváltottad a napi jutalmad!" mt="md">Gyere vissza holnap is!</Notification>}
-            </div>
+            {claimSuccess && (
+                <Notification color="lime" radius="lg" title="Sikeresen kiváltottad a napi jutalmad!" mt="md">
+                    Gyere vissza holnap is!
+                </Notification>
+            )}
             <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder>
                 <Group justify="space-between" mt="lg" mb="xs">
-                    <Text fw={900} size="50px" tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>Napi bejelentkezési bónuszod</Text>
+                    <Text fw={900} size="50px" tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
+                        Napi bejelentkezési bónuszod
+                    </Text>
                     <Badge color="pink">Gyere vissza minden nap!</Badge>
                 </Group>
                 <Space h="lg"></Space>
@@ -44,11 +44,12 @@ export default function LoginStreak() {
                     {monthDays.map((day, index) => (
                         <div
                             key={index}
-                            className={`calendar-cell ${day !== null ? 'active' : 'hidden'} ${userClicked.includes(day) ? 'claimed' : ''} ${day === todayDate ? 'today' : ''}`}
+                            className={`calendar-cell ${day !== null ? 'active' : 'hidden'} ${userClicked.includes(day) ? 'claimed' : ''} ${
+                                day === todayDate ? 'today' : 'past'
+                            }`}
                             onClick={() => handleButtonClick(day)}
                         >
                             {day !== null && day}
-
                         </div>
                     ))}
                 </div>
