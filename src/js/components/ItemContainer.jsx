af4f4e474e37cfdf8a3eed48f855277424c2e48a
Babli04 <orosz.balazs.kevin@gmail.com>
2024-02-26 10:13:24 +0100

    Added +1 rarity, colors changed to csgo item rarity colors.

--- src/js/components/ItemContainer.jsx
+++ src/js/components/ItemContainer.jsx
@@ -1,7 +1,7 @@
 import { Card, Grid, Text } from '@mantine/core';
 import React from 'react';
 
-const rarityColors = ['#afafaf', '#6496e1', '#4b69cd', '#8847ff', '#d32ce6', '#eb4b4b', '#ffcc00']
+const rarityColors = ['#ffffff', '#98c0f5', '#1b7cfa', '#781bfa', '#e71bfa', '#fc3f3f', '#fcb13f', '#fcb13f']
 
 export default function ItemContainer({ item }) {
     return (
