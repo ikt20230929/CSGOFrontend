d401efc4d50796febbca14840c759d8dc743eed6
molnard1 <101341428+molnard1@users.noreply.github.com>
2024-03-02 19:34:12 +0100

    Daily reward claiming
    This resolves #5

--- src/js/pages/LoginStreak.jsx
+++ src/js/pages/LoginStreak.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
-import 'dayjs/locale/hu';
 import { Card, Group, Text, Space, Badge, Notification } from "@mantine/core";
-import CenteredContainer from '../components/CenteredContainer';
-
-dayjs.locale('hu');
+import { useSelector } from 'react-redux';
+import { fetchEndpoint, fetchProfile } from '../Globals';
 
 export default function LoginStreak() {
+    const { profile } = useSelector(state => state.data);
+
     const startOfMonth = dayjs().startOf('month');
     const daysInMonth = startOfMonth.daysInMonth();
     const todayDate = dayjs().date();
@@ -16,46 +16,50 @@ export default function LoginStreak() {
         Array.from({ length: daysInMonth }, (_, i) => i + 1)
     );
 
-    const [userClicked, setUserClicked] = useState([]);
     const [claimSuccess, setClaimSuccess] = useState(false);
+    const [alreadyClaimedToday, setAlreadyClaimedToday] = useState(dayjs(profile.userLastClaimDate).isSame(dayjs(), 'day'));
+    const [amount, setAmount] = useState(0);
 
-    const handleButtonClick = (day) => {
-        if (day === todayDate) {
-            setUserClicked([...userClicked, day]);
+    const handleButtonClick = async () => {
+        var result = await fetchEndpoint('daily');
+
+        if (result.success) {
             setClaimSuccess(true);
+            setAmount(result.data.amount);
+            setAlreadyClaimedToday(true);
         }
+
+        await fetchProfile();
     };
 
     return (
         <>
             {claimSuccess && (
-    <Notification  withCloseButton={false} className='alerteles' withBorder color="cyan" radius="lg" title="Sikeresen kiváltottad a napi jutalmad!">
-    Gyere vissza holnap is!
-  </Notification>
+                <Notification withCloseButton={false} className='alerteles regpage' withBorder color="cyan" radius="lg" title="Sikeresen kiváltottad a napi jutalmad!">
+                    A mai jutalmad: ${amount}!<br />Gyere vissza holnap is!
+                </Notification>
             )}
-        <div className='loginstreak'>
-            <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder>
-                <Group justify="space-between" mt="lg" mb="xs">
-                    <Text fw={900} size="50px" tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
-                        Napi bejelentkezési bónuszod
-                    </Text>
-                    <Badge color="pink">Gyere vissza minden nap!</Badge>
-                </Group>
-                <Space h="lg"></Space>
-                <div className="calendar-grid">
-                    {monthDays.map((day, index) => (
-                        <div
-                            key={index}
-                            className={`calendar-cell ${day !== null ? 'active' : 'hidden'} ${userClicked.includes(day) ? 'claimed' : ''} ${
-                                day === todayDate ? 'today' : 'past'
-                            }`}
-                            onClick={() => handleButtonClick(day)}
-                        >
-                            {day !== null && day}
-                        </div>
-                    ))}
-                </div>
-            </Card>
+            <div className='loginstreak'>
+                <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder>
+                    <Group justify="space-between" mt="lg" mb="xs">
+                        <Text fw={900} size="50px" tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
+                            Napi bejelentkezési bónuszod
+                        </Text>
+                        <Badge color="pink">Gyere vissza minden nap!</Badge>
+                    </Group>
+                    <Space h="lg"></Space>
+                    <div className="calendar-grid">
+                        {monthDays.map((day, index) => (
+                            <div
+                                key={index}
+                                className={`calendar-cell ${day !== null ? 'active' : 'hidden'} ${day !== todayDate || alreadyClaimedToday ? 'past' : 'today'}`}
+                                onClick={!alreadyClaimedToday && day === todayDate ? handleButtonClick : undefined}
+                            >
+                                {day !== null && day}
+                            </div>
+                        ))}
+                    </div>
+                </Card>
             </div>
         </>
     );
