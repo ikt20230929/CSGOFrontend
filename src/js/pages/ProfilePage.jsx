7883baf37ccb96ead8a1d07b8029ee87685bd0cd
IvadiZs <ivadizs@kkszki.hu>
2024-03-28 08:58:55 +0100

    On item select now you can see the possible outcomes

--- src/js/pages/ProfilePage.jsx
+++ src/js/pages/ProfilePage.jsx
@@ -8,124 +8,121 @@ import { API_URL } from "../settings";
 import store from "../store";
 
 export default function ProfilePage() {
-  const { profile, inventory } = useSelector(state => state.data);
-  const [opened, { open, close }] = useDisclosure(false);
-  const [ searchTerm, setSearchTerm ] = useState('');
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [showNotification, setShowNotification] = useState(false);
-  const [confirmSellModal, { open: openConfirmSellModal, close: closeConfirmSellModal }] = useDisclosure(false);
-  const [sellConfirmationModal, { open: openSellConfirmationModal, close: closeSellConfirmationModal }] = useDisclosure(false);
-  const [isInventoryUpdated, setIsInventoryUpdated] = useState(false);
+    const { profile, inventory } = useSelector(state => state.data);
+    const [opened, { open, close }] = useDisclosure(false);
+    const [ searchTerm, setSearchTerm ] = useState('');
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [showNotification, setShowNotification] = useState(false);
+    const [confirmSellModal, { open: openConfirmSellModal, close: closeConfirmSellModal }] = useDisclosure(false);
+    const [sellConfirmationModal, { open: openSellConfirmationModal, close: closeSellConfirmationModal }] = useDisclosure(false);
+    const [isInventoryUpdated, setIsInventoryUpdated] = useState(false);
 
+    const handlebtnclick = () => {
+        setShowNotification(true);
+    };
 
-  const handlebtnclick = () => {
-    setShowNotification(true);
-  }
+    const handleSell = () => {
+        openConfirmSellModal();
+    };
 
-  const handleSell = () => {
-    openConfirmSellModal();
-  };
+    const confirmSell = async () => {
+        closeConfirmSellModal();
+        try {
+            await Promise.all(selectedItems.map(async (element) => {
+                const response = await axios({
+                    method: 'post',
+                    url: `${API_URL}/sell_item/${element}`,
+                    headers: {
+                        'Authorization': `Bearer ${store.getState().auth.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+            }));
+            setIsInventoryUpdated(true);
+            openSellConfirmationModal();
+        } catch (error) {
+            alert("Hiba az eladás során!")
+        }
+    };
 
-  const confirmSell = async () => {
-    closeConfirmSellModal();
-    try {
-      await Promise.all(selectedItems.map(async (element) => {
-        const response = await axios({
-          method: 'post',
-          url: `${API_URL}/sell_item/${element}`,
-          headers: {
-              'Authorization': `Bearer ${store.getState().auth.accessToken}`,
-              'Content-Type': 'application/json'
-          }
-        });
-        
-      }));
-      setIsInventoryUpdated(true);
-      openSellConfirmationModal();
-    } catch (error) {
-      alert("Hiba az eladás során!")
-    }
-  };
+    const toggleItemSelection = (itemId) => {
+        if (selectedItems.includes(itemId)) {
+            setSelectedItems(selectedItems.filter((id) => id !== itemId));
+        } else {
+            setSelectedItems([...selectedItems, itemId]);
+        }
+    };
 
-  const toggleItemSelection = (itemId) => {
-    if (selectedItems.includes(itemId)) {
-      setSelectedItems(selectedItems.filter((id) => id !== itemId));
-    } else {
-      setSelectedItems([...selectedItems, itemId]);
-    }
-  };
+    useEffect(() => {
+        if (isInventoryUpdated) {
+            setIsInventoryUpdated(false);
+            setTimeout(() => {
+                window.location.reload();
+            }, 1000);
+        }
+    }, [isInventoryUpdated]);
 
-  useEffect(() => {
-    if (isInventoryUpdated) {
-      setIsInventoryUpdated(false);
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
-    }
-  }, [isInventoryUpdated]);
-  
-
-  return (
-    <div>
-        <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder style={{minHeight: "calc(100vh - 3.5rem)"}}>
-          <Group justify="space-between" mt="lg" mb="xs">
-            <Text size='90px' fw={700} tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(143, 143, 143, 1)', deg: 90 }}>
-              {profile.userName} profilja
-            </Text>
-          </Group>
-          <Space h="sm"/>
-          <Text size="xl">Egyenleged: <NumberFormatter prefix="$" fixedDecimalScale={true} decimalScale={2} value={profile.userBalance} /></Text>
-          <Space h="xl" />
-          <Modal opened={opened} onClose={close} title="Adataid szerkesztése" transitionProps={{ transition: 'pop', duration: 400, timingFunction: 'ease' }}>
-            <TextInput label="Steam URL módosítása" placeholder="https://steamcommunity.com/tradeoffer/new/?partner=yourowntoken" />
-            <TextInput label="Partner kód" placeholder="FREE3USD" />
-            <FileInput variant="filled" label="Profilkép" description="Válassz magadnak egy új profilképet!" placeholder="Katt!" />
-            <Space h="md" />
-            <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={close}>
-              Mentés
-            </Button>
-          </Modal>
-          <Button onClick={open} variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }}>
-            Adatok módosítása
-          </Button>
-          <Space h="xs"></Space>
-          <Button onClick={() => handleSell(selectedItems)} variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }}>
-            Kiválasztott tárgyak eladása
-          </Button>
-          <Space h="xs"></Space>
-          <Button onClick={handlebtnclick} variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }}>
-            Kiválasztott tárgyak kikérése
-          </Button>
-          <Center>
-            {showNotification && (
-              <Notification withCloseButton={true} className='openalert' withBorder color="cyan" radius="lg" title="A kiválaszott tárgyak hamarosan a Steam csere kérelmeid között lesznek!" />
-            )}
-          </Center>
-          <Group justify="space-between">
-            <h2 style={{ color: 'white' }}>Megszerzett tárgyak: ({inventory.length} db)</h2>
-            <TextInput placeholder="Keresés" classNames={{ input: 'regpage' }} onChange={event => setSearchTerm(event.currentTarget.value)} />
-          </Group>
-          <Grid gutter="lg">
-            <InventorySearchWrapper searchTerm={searchTerm} items={[...inventory].sort((a, b) => b.itemRarity - a.itemRarity)} onToggleItem={toggleItemSelection} />
-          </Grid>
-          <Modal opened={confirmSellModal} onClose={closeConfirmSellModal} title="Eladás megerősítése" transitionProps={{ transition: 'pop', duration: 400, timingFunction: 'ease' }}>
-            <Text>Biztosan el akarod adni a kiválasztott tárgyakat?</Text>
-            <Space h="md" />
-            <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={confirmSell}>
-              Eladás
-            </Button>
-            <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={closeConfirmSellModal}>
-              Mégse
-            </Button>
-          </Modal>
-          <Modal opened={sellConfirmationModal} onClose={closeSellConfirmationModal} title="Eladás megerősítve" transitionProps={{ transition: 'pop', duration: 400, timingFunction: 'ease' }}>
-            <Text>Az eladás sikeresen megtörtént!</Text>
-            <Space h="md" />
-            <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={closeSellConfirmationModal}>
-              Ok
-            </Button>
-          </Modal>
-        </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder style={{minHeight: "calc(100vh - 3.5rem)"}}>
+                <Group justify="space-between" mt="lg" mb="xs">
+                    <Text size='90px' fw={700} tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(143, 143, 143, 1)', deg: 90 }}>
+                        {profile.userName} profilja
+                    </Text>
+                </Group>
+                <Space h="sm"/>
+                <Text size="xl">Egyenleged: <NumberFormatter prefix="$" fixedDecimalScale={true} decimalScale={2} value={profile.userBalance} /></Text>
+                <Space h="xl" />
+                <Modal opened={opened} onClose={close} title="Adataid szerkesztése" transitionProps={{ transition: 'pop', duration: 400, timingFunction: 'ease' }}>
+                    <TextInput label="Steam URL módosítása" placeholder="https://steamcommunity.com/tradeoffer/new/?partner=yourowntoken" />
+                    <TextInput label="Partner kód" placeholder="FREE3USD" />
+                    <FileInput variant="filled" label="Profilkép" description="Válassz magadnak egy új profilképet!" placeholder="Katt!" />
+                    <Space h="md" />
+                    <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={close}>
+                        Mentés
+                    </Button>
+                </Modal>
+                <Button onClick={open} variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }}>
+                    Adatok módosítása
+                </Button>
+                <Space h="xs"></Space>
+                <Button onClick={() => handleSell(selectedItems)} variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }}>
+                    Kiválasztott tárgyak eladása
+                </Button>
+                <Space h="xs"></Space>
+                <Button onClick={handlebtnclick} variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }}>
+                    Kiválasztott tárgyak kikérése
+                </Button>
+                <Center>
+                    {showNotification && (
+                        <Notification withCloseButton={true} className='openalert' withBorder color="cyan" radius="lg" title="A kiválaszott tárgyak hamarosan a Steam csere kérelmeid között lesznek!" />
+                    )}
+                </Center>
+                <Group justify="space-between">
+                    <h2 style={{ color: 'white' }}>Megszerzett tárgyak: ({inventory.length} db)</h2>
+                    <TextInput placeholder="Keresés" classNames={{ input: 'regpage' }} onChange={event => setSearchTerm(event.currentTarget.value)} />
+                </Group>
+                <Grid gutter="lg">
+                    <InventorySearchWrapper searchTerm={searchTerm} items={[...inventory].sort((a, b) => b.itemRarity - a.itemRarity)} onToggleItem={toggleItemSelection} />
+                </Grid>
+                <Modal opened={confirmSellModal} onClose={closeConfirmSellModal} title="Eladás megerősítése" transitionProps={{ transition: 'pop', duration: 400, timingFunction: 'ease' }}>
+                    <Text>Biztosan el akarod adni a kiválasztott tárgyakat?</Text>
+                    <Space h="md" />
+                    <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={confirmSell}>
+                        Eladás
+                    </Button>
+                    <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={closeConfirmSellModal}>
+                        Mégse
+                    </Button>
+                </Modal>
+                <Modal opened={sellConfirmationModal} onClose={closeSellConfirmationModal} title="Eladás megerősítve" transitionProps={{ transition: 'pop', duration: 400, timingFunction: 'ease' }}>
+                    <Text>Az eladás sikeresen megtörtént!</Text>
+                    <Space h="md" />
+                    <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 90 }} type="submit" onClick={closeSellConfirmationModal}>
+                        Ok
+                    </Button>
+                </Modal>
+            </Card>
+        </div>
+    );
 }
--- src/js/pages/SkinUpgradePage.jsx
+++ src/js/pages/SkinUpgradePage.jsx
@@ -5,13 +5,15 @@ import { useSelector } from "react-redux";
 import InventorySearchWrapper from "../components/InventorySearchWrapper";
 import {Link} from 'react-router-dom';
 import { fetchEndpoint } from '../Globals';
+import axios from 'axios';
+import store from '../store';
+import { API_URL } from '../settings';
 
 const MultiplierWheel = () => {
-    const { profile, inventory } = useSelector(state => state.data);
-    const [amount, setAmount] = useState(profile.userBalance);
-    const [winningAmount, setWinningAmount] = useState(profile.userBalance * 50); 
+    const { profile, inventory } = useSelector(state => state.data); 
     const [ searchTerm, setSearchTerm ] = useState('');
     const [ allItems, setAllItems ] = useState([]);
+    const [selectedItems, setSelectedItems] = useState([]);
 
     const [spinTrigger, setSpinTrigger] = useState(false);
 
@@ -23,96 +25,115 @@ const MultiplierWheel = () => {
         setSpinTrigger(false);
     }, []);
 
-    useEffect(() => {
-      fetchItems = async () => {
-        var items = (await fetchEndpoint("items")).data;
-        setAllItems(items);
+    const chance = 0;
+
+    const toggleItemSelection = (itemId) => {
+      let newSelectedItems;
+      if (selectedItems.includes(itemId)) {
+          newSelectedItems = selectedItems.filter((id) => id !== itemId);
+      } else {
+          newSelectedItems = [...selectedItems, itemId];
       }
+      setSelectedItems(newSelectedItems);
+      if (newSelectedItems.length >= 1) {
+          fetchUpgradeItems(newSelectedItems);
+      }
+  };
 
-      fetchItems();
-    }, []);
-
-    // Esély kiszámítása
-    const chance = (0.75 + (winningAmount - (amount + 0.1)) / (amount * 50 - (amount + 0.1)) * (0.01 - 0.75)) * 100;
-
-    const handleAmountChange = (value) => {
-        setAmount(value); 
-        setWinningAmount(amount + 0.1); // Nyeremény csúszka ugrása a minimumhoz
+    const fetchUpgradeItems = async (itemsToUpgrade) => {
+      try {
+          const response = await axios({
+              method: 'post',
+              url: `${API_URL}/items/upgrades`,
+              headers: {
+                  'Authorization': `Bearer ${store.getState().auth.accessToken}`,
+                  'Content-Type': 'application/json'
+              },
+              data: {
+                  Items: itemsToUpgrade,
+                  Multiplier: 1
+              }
+          });
+          const items = response.data.items.map(obj => obj.item);
+          setAllItems(items);
+      } catch (error) {
+          
+      }
     };
 
     return (
         <div style={{width:"100%"}}>
-            
-                <Text size='90px' fw={700} tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
-                    Skin Upgrader
-                </Text>
-                <FortuneWheel number={chance} spinTrigger={spinTrigger} resetSpinTrigger={resetSpinTrigger} />
-                <Center>
+            <Text size='90px' fw={700} tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
+                Skin Upgrader
+            </Text>
+            <FortuneWheel number={chance} spinTrigger={spinTrigger} resetSpinTrigger={resetSpinTrigger} />
+            <Center>
                 <Grid align='center' justify="center">
-      <Grid.Col span={5}>
-        <Center>
-      <Card className="upgrdcard" shadow="sm" padding="lg" radius="md" withBorder style={{ textAlign: 'center', minHeight: "calc(380px - 1.5rem)" }}>
-                <Text size='20px' fw={700}   tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
-                    A te tárgyaid, {profile.userName} ({inventory.length} db)
-                    </Text> 
-                    <Space h="xs"></Space>
-                    <Group justify="space-between">
-            <TextInput placeholder="Keresés" classNames={{ input: 'regpage' }} onChange={event => setSearchTerm(event.currentTarget.value)} />
-          </Group>
-          <Space h="xs"></Space>
-          <Grid gutter="lg" direction="row" style={{overflowY: 'auto', overflowX: 'hidden'}}>
-            <InventorySearchWrapper searchTerm={searchTerm} items={[...inventory].sort((a, b) => b.itemRarity - a.itemRarity)} />
-            </Grid>
-          </Card>
-          </Center>
-
-      </Grid.Col>
-      <Grid.Col span={2}>
-        <Center>
-        <Space h="xl"></Space>
-      <Button fullWidth
-                        variant="gradient"
-                        style={{ marginTop: 20 }}
-                        onClick={triggerSpin}
-                    >
-                        UPGRADE
-                    </Button>
-                    </Center>
-                    <Space h="xs"></Space>
-                    <Center>
-                        <Link to="/upgrader">
-                    <Button fullWidth
-                        variant="gradient"
-                        style={{ marginTop: 20 }}
-                        onClick={() => {
-                            
-                        }}
-                    >
-                        Balance upgrade
-                    </Button>
-                    </Link>
-                    </Center>
-      </Grid.Col>
-      <Grid.Col span={5}>
-<Center>
-      <Card className="upgrdcard2" shadow="sm" padding="lg" radius="md" withBorder style={{ textAlign: 'center', minHeight: "calc(380px - 1.5rem)" }}>
-                <Text size='20px' fw={700} tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
-                    A mi tárgyaink
-                    </Text>
-                    <Space h="xs"></Space>
-                    <Group justify="space-between">
-            <TextInput placeholder="Keresés" classNames={{ input: 'regpage' }} onChange={event => setSearchTerm(event.currentTarget.value)} />
-          </Group>
-          <Space h="xs"></Space>
-          <Grid gutter="lg" direction="row" style={{overflowY: 'auto', overflowX: 'hidden'}}>
-            <InventorySearchWrapper searchTerm={searchTerm} items={[...allItems].sort((a, b) => b.itemRarity - a.itemRarity)} />
-          </Grid>
-                </Card>
-                </Center>
-      </Grid.Col>
+                    <Grid.Col span={5}>
+                        <Center>
+                            <Card className="upgrdcard" shadow="sm" padding="lg" radius="md" withBorder style={{ textAlign: 'center', minHeight: "calc(380px - 1.5rem)" }}>
+                                <Text size='20px' fw={700}   tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
+                                    A te tárgyaid, {profile.userName} ({inventory.length} db)
+                                </Text> 
+                                <Space h="xs"></Space>
+                                <Group justify="space-between">
+                                    <TextInput placeholder="Keresés" classNames={{ input: 'regpage' }} onChange={event => setSearchTerm(event.currentTarget.value)} />
+                                </Group>
+                                <Space h="xs"></Space>
+                                <Grid gutter="lg" direction="row" style={{overflowY: 'auto', overflowX: 'hidden'}}>
+                                    <InventorySearchWrapper searchTerm={searchTerm} items={[...inventory].sort((a, b) => b.itemRarity - a.itemRarity)} onToggleItem={toggleItemSelection}/>
+                                </Grid>
+                            </Card>
+                        </Center>
+                    </Grid.Col>
+                    <Grid.Col span={2}>
+                        <Center>
+                            <Space h="xl"></Space>
+                            <Button fullWidth
+                                    variant="gradient"
+                                    style={{ marginTop: 20 }}
+                                    onClick={triggerSpin}
+                            >
+                                UPGRADE
+                            </Button>
+                        </Center>
+                        <Space h="xs"></Space>
+                        <Center>
+                            <Link to="/upgrader">
+                                <Button fullWidth
+                                        variant="gradient"
+                                        style={{ marginTop: 20 }}
+                                        onClick={() => {
+                                            
+                                        }}
+                                >
+                                    Balance upgrade
+                                </Button>
+                            </Link>
+                        </Center>
+                    </Grid.Col>
+                    <Grid.Col span={5}>
+                        <Center>
+                            <Card className="upgrdcard2" shadow="sm" padding="lg" radius="md" withBorder style={{ textAlign: 'center', minHeight: "calc(380px - 1.5rem)" }}>
+                                <Text size='20px' fw={700} tt="uppercase" variant="gradient" gradient={{ from: 'rgba(255, 255, 255, 1)', to: 'rgba(99, 234, 255, 1)', deg: 90 }}>
+                                    Lehetséges nyeremények
+                                </Text>
+                                <Space h="xs"></Space>
+                                <Group justify="space-between">
+                                    <TextInput placeholder="Keresés" classNames={{ input: 'regpage' }} onChange={event => setSearchTerm(event.currentTarget.value)} />
+                                </Group>
+                                <Space h="xs"></Space>
+                                {selectedItems.length > 0 && (
+                                  <Grid gutter="lg" direction="row" style={{overflowY: 'auto', overflowX: 'hidden'}}>
+                                      <InventorySearchWrapper searchTerm={searchTerm} items={[...allItems].sort((a, b) => b.itemRarity - a.itemRarity)} />
+                                  </Grid>
+                              )}
+                            </Card>
+                        </Center>
+                    </Grid.Col>
                 </Grid>
-                </Center>
-                </div>
+            </Center>
+        </div>
     );
 };
 
