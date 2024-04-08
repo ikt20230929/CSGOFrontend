abeabcb1e7ef0895a020e0e6c9cbfee025184050
Babli04 <orosz.balazs.kevin@gmail.com>
2024-04-08 10:14:14 +0200

    Homepage now responsive

--- src/js/pages/HomePage.jsx
+++ src/js/pages/HomePage.jsx
@@ -20,22 +20,23 @@ export default function MainPage() {
   };
 
   return (
-    <CenteredContainer size="xl">
-      <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder style={{ width: "100vw" }}>
+      <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder style={{ width: "100%", maxWidth: "100%" }}>
         <Group justify="space-between" mt="md" mb="xs">
           <Text fw={500}>Tavaszi ládák</Text>
           <Badge color="pink">Összes láda: ({cases.length} db)</Badge>
         </Group>
         <Grid gutter="lg">
           {cases.map(_case => (
-            <Grid.Col span={3} key={_case.caseId} onClick={() => handleCaseClick(_case.caseId)}>
+            <Grid.Col span={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={_case.caseId} onClick={() => handleCaseClick(_case.caseId)}>
               <Card className="regpage" shadow="sm" padding="lg" radius="md" withBorder onMouseEnter={() => handleMouseEnter(_case.caseId)}
               onMouseLeave={handleMouseLeave} style={{ 
                 cursor: 'pointer', 
-                border: hoveredCard === _case.caseId ? '1px solid aqua' : '1px solid #cbd5e0' 
+                border: hoveredCard === _case.caseId ? '1px solid aqua' : '1px solid #cbd5e0' ,
+                width: "100%", maxWidth: "100%"
               }}>
                 <Text>{_case.caseName}</Text>
                 <Text size="sm" color="dimmed">({_case.items.length} db)</Text>
+                <Text size="sm" color="dimmed" fw={500}>{_case.itemValue} $</Text>
                 {/*{_case.items.map(_caseItem => (
                   <Text key={_caseItem.itemId} size="sm" color="dimmed">{_caseItem.itemName} - {_caseItem.itemSkinName} (Ár: {_caseItem.itemSkinValue} $)</Text>
                 ))}*/}
@@ -44,6 +45,5 @@ export default function MainPage() {
           ))}
         </Grid>
       </Card>
-    </CenteredContainer>
   );
 }
